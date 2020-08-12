e7cdbea6f53396f556de5978eb2a6328425f698f
zooj <jie.zhang@cisdi.com.cn>
2020-08-12 16:49:47 +0800

    Initial commit

---
+++ children-app-1/src/views/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h2>This is subApp1</h2>
+    <a href="/app2/">go to App2</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
