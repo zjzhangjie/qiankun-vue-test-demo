4db2d4075966c99e8c781f78b4847f94b26f0bbd
zooj <jie.zhang@cisdi.com.cn>
2020-08-13 17:00:55 +0800

    fix

---
+++ children-app-1/src/views/menu1.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="block">
+    <el-timeline>
+        <el-timeline-item timestamp="2018/4/12" placement="top">
+            <el-card>
+                <h4>更新 Github 模板</h4>
+                <p>王小虎 提交于 2018/4/12 20:46</p>
+            </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2018/4/3" placement="top">
+            <el-card>
+                <h4>更新 Github 模板</h4>
+                <p>王小虎 提交于 2018/4/3 20:46</p>
+            </el-card>
+        </el-timeline-item>
+        <el-timeline-item timestamp="2018/4/2" placement="top">
+            <el-card>
+                <h4>更新 Github 模板</h4>
+                <p>王小虎 提交于 2018/4/2 20:46</p>
+            </el-card>
+        </el-timeline-item>
+    </el-timeline>
+</div>
+</template>
