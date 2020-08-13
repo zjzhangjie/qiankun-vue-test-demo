dac4ebbf101f403619f23de5ac48dee6dab5d07c
zooj <jie.zhang@cisdi.com.cn>
2020-08-13 16:03:48 +0800

    提交header

---
+++ register-model/src/components/commom/header.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>查看</el-dropdown-item>
+                <el-dropdown-item>新增</el-dropdown-item>
+                <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        <span>王小虎</span>
+    </el-header>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style scoped>
+    .el-header{
+        background-color:#303133 ;
+        color: #ffffff;
+        text-align: center;
+    }
+
+</style>
