2307d714cf5fd4535d34e5fd99bbaeaa14aceb80
zooj <jie.zhang@cisdi.com.cn>
2020-08-14 16:02:47 +0800

    提交header

--- register-model/src/components/commom/header.vue
+++ register-model/src/components/commom/header.vue
@@ -1,29 +1,38 @@
 <template>
-    <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
-            <i class="el-icon-setting" style="margin-right: 15px"></i>
-            <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>查看</el-dropdown-item>
-                <el-dropdown-item>新增</el-dropdown-item>
-                <el-dropdown-item>删除</el-dropdown-item>
-            </el-dropdown-menu>
-        </el-dropdown>
-        <span>王小虎</span>
-    </el-header>
+  <el-header style="text-align: right; font-size: 12px">
+    <el-dropdown>
+      <i class="el-icon-setting" style="margin-right: 15px"></i>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>查看</el-dropdown-item>
+        <el-dropdown-item>新增</el-dropdown-item>
+        <el-dropdown-item>删除</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <span>{{userName}}</span>
+  </el-header>
 </template>
 
 <script>
 export default {
   name: 'qiankun-header',
+  data() {
+    return {
+      userName: 'admin',
+    };
+  },
+  computed: {
+
+  },
+  mounted() {},
+  methods: {},
 };
 </script>
 
 <style scoped>
-    .el-header{
-        background-color:#303133 ;
-        line-height: 60px;
-        color: #ffffff;
-        text-align: center;
-    }
-
+.el-header {
+  background-color: #303133;
+  line-height: 60px;
+  color: #ffffff;
+  text-align: center;
+}
 </style>
