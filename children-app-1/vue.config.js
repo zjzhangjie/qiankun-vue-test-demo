14994af0b335bf49b7a36a949063288447d7c0c0
zooj <jie.zhang@cisdi.com.cn>
2020-08-24 10:40:24 +0800

    初始化

--- children-app-1/vue.config.js
+++ children-app-1/vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { name } = require('./package');
 
 function resolve(dir) {
-	return path.join(__dirname, dir);
+  return path.join(__dirname, dir);
 }
 
 const port = 8092; // dev port
@@ -15,36 +15,36 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-	outputDir: 'dist',
-	assetsDir: 'static',
-	filenameHashing: true,
-	// tweak internal webpack configuration.
-	// see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-	devServer: {
-		// host: '0.0.0.0',
-		hot: true,
-		disableHostCheck: true,
-		port,
-		overlay: {
-			warnings: false,
-			errors: true,
-		},
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-		},
-	},
-	// 自定义webpack配置
-	configureWebpack: {
-		resolve: {
-			alias: {
-				'@': resolve('src'),
-			},
-		},
-		output: {
-			// 把子应用打包成 umd 库格式
-			library: `${name}-[name]`,
-			libraryTarget: 'umd',
-			jsonpFunction: `webpackJsonp_${name}`,
-		},
-	},
+  outputDir: 'children-app-1',
+  assetsDir: 'static',
+  filenameHashing: true,
+  // tweak internal webpack configuration.
+  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  devServer: {
+    // host: '0.0.0.0',
+    hot: true,
+    disableHostCheck: true,
+    port,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  // 自定义webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+    output: {
+      // 把子应用打包成 umd 库格式
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${name}`,
+    },
+  },
 };
