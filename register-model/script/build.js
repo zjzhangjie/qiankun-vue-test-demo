22584fd392312c437869ecfa9cb0afe4a6f96c01
zooj <jie.zhang@cisdi.com.cn>
2020-10-13 14:14:29 +0800

    add:打包配置文件

---
+++ register-model/script/build.js
@@ -0,0 +1,13 @@
+/**
+ * 打包时将各个应用打包的文件拷贝到一个文件夹中
+ */
+
+const fs = require('fs-extra');
+const path = require('path');
+
+const outputDir = 'qiankun';// 打包后得文件名
+
+// 拷贝文件
+fs.copySync(path.join(process.cwd(), './register-model'), path.join(process.cwd(), outputDir, 'register-model'));
+fs.copySync(path.join(process.cwd(), '../children-app-1/children-app-1'), path.join(process.cwd(), outputDir, 'children-app-1'));
+fs.copySync(path.join(process.cwd(), '../children-app-2/children-app-2'), path.join(process.cwd(), outputDir, 'children-app-2'));
