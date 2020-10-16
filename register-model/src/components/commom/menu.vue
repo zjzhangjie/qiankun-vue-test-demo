01662f0c0f1f2286d20e00d272cfabc21b53fa8f
zooj <jie.zhang@cisdi.com.cn>
2020-10-16 10:22:33 +0800

    提交header

--- register-model/src/components/commom/menu.vue
+++ register-model/src/components/commom/menu.vue
@@ -4,8 +4,8 @@
             <template slot="title"><i class="el-icon-message"></i>菜单</template>
             <el-menu-item-group>
                 <template slot="title">应用1</template>
-                <el-menu-item index="1-1"><a href="/children-app-1/">子菜单1</a></el-menu-item>
-                <el-menu-item index="1-2"><a href="/children-app-1/menu2">子菜单2</a></el-menu-item>
+                <el-menu-item index="1-1"><a href="/qiankun/children-app-1/">子菜单1</a></el-menu-item>
+                <el-menu-item index="1-2"><a href="/qiankun/children-app-1/menu2">子菜单2</a></el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="应用2">
                 <el-menu-item index="2-1"><a href="/children-app-2/">子菜单1</a></el-menu-item>
