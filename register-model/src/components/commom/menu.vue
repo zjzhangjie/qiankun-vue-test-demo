e17cc44dc2bd87cee9e7265b035dcbb88035b13f
zooj <jie.zhang@cisdi.com.cn>
2020-10-16 09:44:38 +0800

    fix :组件名称

--- register-model/src/components/commom/menu.vue
+++ register-model/src/components/commom/menu.vue
@@ -4,12 +4,12 @@
             <template slot="title"><i class="el-icon-message"></i>菜单</template>
             <el-menu-item-group>
                 <template slot="title">应用1</template>
-                <el-menu-item index="1-1"><a href="/qiankun/children-app-1/">子菜单1</a></el-menu-item>
-                <el-menu-item index="1-2"><a href="/qiankun/children-app-1/menu2">子菜单2</a></el-menu-item>
+                <el-menu-item index="1-1"><a href="/children-app-1/">子菜单1</a></el-menu-item>
+                <el-menu-item index="1-2"><a href="/children-app-1/menu2">子菜单2</a></el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="应用2">
-                <el-menu-item index="2-1"><a href="/qiankun/children-app-2/">子菜单1</a></el-menu-item>
-                <el-menu-item index="2-2"><a href="/qiankun/children-app-2/menu2">子菜单2</a></el-menu-item>
+                <el-menu-item index="2-1"><a href="/children-app-2/">子菜单1</a></el-menu-item>
+                <el-menu-item index="2-2"><a href="/children-app-2/menu2">子菜单2</a></el-menu-item>
             </el-menu-item-group>
         </el-submenu>
     </el-menu>
@@ -17,7 +17,7 @@
 
 <script>
 export default {
-  name: 'menu',
+  name: 'menu-test1',
   data() {
     return {
       activeIndex: '1-1',
