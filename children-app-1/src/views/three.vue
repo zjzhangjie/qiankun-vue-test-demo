7ba9749191e8e166e601ba7119d7172835855cbc
zooj <jie.zhang@cisdi.com.cn>
2020-10-21 10:40:07 +0800

    add:three.js

---
+++ children-app-1/src/views/three.vue
@@ -0,0 +1,24 @@
+<!--three.js-->
+<template>
+  <div>
+      <threeTest></threeTest>
+  </div>
+</template>
+
+<script>
+import threeTest from '@/components/three.vue';
+
+export default {
+  name: 'three',
+  methods: {
+
+  },
+  components: {
+    threeTest,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
