4c7ab75a25729ccc946ac01d78bf18f158f1fd5d
zooj <jie.zhang@cisdi.com.cn>
2020-11-25 15:49:55 +0800

    调整配置

--- register-model/src/components/commom/menu.vue
+++ register-model/src/components/commom/menu.vue
@@ -8,8 +8,8 @@
                 <el-menu-item index="1-2"><a href="/qiankun/children-app-1/menu2">子菜单2</a></el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="应用2">
-                <el-menu-item index="2-1"><a href="/children-app-2/">子菜单1</a></el-menu-item>
-                <el-menu-item index="2-2"><a href="/children-app-2/menu2">子菜单2</a></el-menu-item>
+                <el-menu-item index="2-1"><a href="/qiankun/children-app-2/">子菜单1</a></el-menu-item>
+                <el-menu-item index="2-2"><a href="/qiankun/children-app-2/menu2">子菜单2</a></el-menu-item>
             </el-menu-item-group>
         </el-submenu>
     </el-menu>
--- register-model/src/register/index.js
+++ register-model/src/register/index.js
@@ -4,11 +4,22 @@
  */
 import { registerMicroApps, setDefaultMountApp, start, runAfterFirstMounted, addGlobalUncaughtErrorHandler } from 'qiankun';
 import loading from '../progress/index';
+
 import { props, initGlState } from '@/share/';
-import apps from './apps';
+import { apps, defaultActiveRule } from './apps';
 // 定义全局状态
 initGlState();
-
+const baseurl = `${process.env.BASE_URL}`;
+/**
+ * 重构apps
+ */
+function filterApps() {
+  apps.forEach((item) => {
+    item.props = props; // 可选，主应用需要传递给微应用的数据。
+    item.activeRule = genActiveRule(baseurl + item.activeRule);
+  });
+  return apps;
+}
 /**
  * 注册子应用 registerMicroApps(apps,lifeCycles)
  *apps - Array<RegistrableApp> - 必选，微应用的一些注册信息
@@ -44,7 +55,7 @@ function registerApps() {
     },
   );
   // 设置默认子应用,与 genActiveRule中的参数保持一致
-  setDefaultMountApp('/qiankun/children-app-1');
+  setDefaultMountApp(baseurl + defaultActiveRule);
   // 第一个微应用 mount 后需要调用的方法，比如开启一些监控或者埋点脚本。
   runAfterFirstMounted(() => console.log('开启监控'));
   // 添加全局的未捕获异常处理器。
@@ -66,15 +77,5 @@ function registerApps() {
  */
 function genActiveRule(routerPrefix) {
   return location => location.pathname.startsWith(routerPrefix);
-}
-/**
- * 重构apps
- */
-function filterApps() {
-  apps.forEach((item) => {
-    item.props = props; // 可选，主应用需要传递给微应用的数据。
-    item.activeRule = genActiveRule(item.activeRule);
-  });
-  return apps;
 }
 export default registerApps;
