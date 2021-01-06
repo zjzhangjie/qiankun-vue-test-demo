d73d24d8fb67a9f2ba3d805bf6562f3ae14271d5
zooj <jie.zhang@cisdi.com.cn>
2021-01-06 13:53:27 +0800

    fix：去除多余的

--- children-app-1/src/router.js
+++ children-app-1/src/router.js
@@ -3,27 +3,20 @@ import VueRouter from 'vue-router';
 
 
 Vue.use(VueRouter);
-const prefix = '';
-// 判断是 qiankun 环境则增加路由前缀
-if (window.__POWERED_BY_QIANKUN__) {
-  // prefix = '/children-app-1';
-}
 
 const routes = [
   {
-    path: `${prefix}/`,
+    path: '/',
     name: 'menu1',
     component: () => import(/* webpackChunkName: "about" */ '@/views/menu1.vue'),
   },
   {
-    path: `${prefix}/menu2`,
+    path: '/menu2',
     name: 'menu2',
     component: () => import(/* webpackChunkName: "about" */ '@/views/menu2.vue'),
   },
 ];
 const basePath = '/qiankun/';
-console.log(process);
-console.log(process.env.npm_package_name);
 const router = new VueRouter({
   base: window.__POWERED_BY_QIANKUN__ ? `${basePath}children-app-1` : '/',
   mode: 'history',
