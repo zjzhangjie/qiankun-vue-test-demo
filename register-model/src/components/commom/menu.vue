08c14e139aa78d9a34cb19c16a5cccb680b6f806
ranyuan <952885959@qq.com>
2021-01-13 17:37:24 +0800

    ry-2021-1-13

--- register-model/src/components/commom/menu.vue
+++ register-model/src/components/commom/menu.vue
@@ -1,21 +1,31 @@
 <template>
-    <el-menu :default-openeds="['1']"   :default-active="activeIndex">
-        <el-submenu index="1">
-            <template slot="title">qiankun+vue2</template>
-            <el-menu-item-group>
-                <template slot="title">应用1</template>
-                <el-menu-item index="1-1"><router-link :to="{path:'/children-app-1'}">子菜单1</router-link></el-menu-item>
-                <el-menu-item index="1-2"><router-link :to="{path:'children-app-1/menu2'}">子菜单2</router-link></el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="应用2">
-                <el-menu-item index="2-1"><router-link to="/children-app-2">子菜单1</router-link></el-menu-item>
-                <el-menu-item index="2-2"><router-link to="/children-app-2/menu2">子菜单2</router-link></el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="主应用">
-                <el-menu-item index="3-1"><router-link :to="{name:'example'}">子菜单1</router-link></el-menu-item>
-            </el-menu-item-group>
-        </el-submenu>
-    </el-menu>
+  <el-menu :default-openeds="['1']" :default-active="activeIndex">
+    <el-submenu index="1">
+      <template slot="title">qiankun+vue2</template>
+      <el-menu-item-group>
+        <template slot="title">应用1</template>
+        <el-menu-item index="1-1">
+          <router-link :to="{path:'/children-app-1'}">子菜单1</router-link>
+        </el-menu-item>
+        <el-menu-item index="1-2">
+          <router-link :to="{path:'/children-app-1/menu2'}">子菜单2</router-link>
+        </el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="应用2">
+        <el-menu-item index="2-1">
+          <router-link to="/children-app-2">子菜单1</router-link>
+        </el-menu-item>
+        <el-menu-item index="2-2">
+          <router-link :to="{path:'/children-app-2/menu2'}">子菜单2</router-link>
+        </el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="主应用">
+        <el-menu-item index="3-1">
+          <router-link :to="{name:'example'}">子菜单1</router-link>
+        </el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
 </template>
 
 <script>
@@ -30,5 +40,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
